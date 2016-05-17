b5ef8870796fa9a974b22e6b70045c9845b2d9a4
Mourdjen <liaroc13@gmail.com>
2016-05-17 09:58:31 +0200

    add popups for tutorials and modifie win scene for permette restart of the game

---
+++ Assets/Scripts/WinButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinButton : MonoBehaviour {
+
+	private int widthBox = 150;
+	private int heightBox = 40;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(Screen.width/2 - widthBox/2, Screen.height/2 - heightBox/2, widthBox, heightBox), "You win! \n click to restart"))
+			SceneManager.LoadScene(0);
+	}
+}
---
+++ Assets/Scripts/tutorial.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorial : MonoBehaviour {
+	public string text = "this is the tutorial"; // you can put a \n to add a line
+	public int secondsForRead = 3;
+	public int widthBox = 300;
+	public int heightBox = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if(Time.time <= secondsForRead){
+			GUI.Box(new Rect(200, 200, widthBox, heightBox), text);
+		}
+	}
+}
