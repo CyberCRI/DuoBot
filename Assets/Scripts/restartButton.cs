2c180a0cf34012625e22f815a9d71059de7a6915
Mourdjen <liaroc13@gmail.com>
2016-05-17 09:05:51 +0200

    add restart level button and remove click input (only right-ctrl stay)

---
+++ Assets/Scripts/restartButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class restartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 100, 30), "restart level"))
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
